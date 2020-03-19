860cd8bb5566204822e10b861f4fe21f13d567f8
palu3492 <palu3492@stthomas.edu>
2020-03-19 18:39:03 -0500

    Updated printing scheduler stats

--- src/main.cpp
+++ src/main.cpp
@@ -320,12 +320,10 @@ void clearOutput(int num_lines)
 void printSchedulerStatistics(std::vector<Process*>& processes, uint32_t start)
 {
 	uint32_t end = currentTime();
-	uint32_t total_time = end - start;
+    uint32_t total_time = end - start;
 	double total_turn_time = 0;
 	double total_wait_time = 0;
 	double total_cpu_time = 0;
-	// Throughput - number of processes executed by the CPU in a given amount of time
-	// total time / # processes
 	int i;
 	for (i = 0; i < processes.size(); i++)
 	{
@@ -334,15 +332,17 @@ void printSchedulerStatistics(std::vector<Process*>& processes, uint32_t start)
         total_cpu_time += processes[i]->getCpuTime();
 	}
     printf("\nScheduler Statistics: \n");
-    printf("Total time: %f \n", total_time/1000.0);
-    printf("Total turn time: %f \n", total_turn_time);
-    printf("CPU utilization: %f \n", total_time/total_cpu_time);
+    printf("(test) Total time: %f \n", total_time/1000.0);
+    printf("(test) Total turn time: %f \n", total_turn_time);
+    printf("CPU utilization: %f%% \n", (total_cpu_time/total_turn_time)*100);
     printf("Throughput:\n");
+    // Throughput is the number of processes executed by the CPU in a given amount of time
     printf("\tAverage for first 50%% of processes finished: \n");
     printf("\tAverage for second 50%% of processes finished: \n");
-    printf("\tOverall average: %f \n", total_time/processes.size());
-    printf("Average turnaround time: %f \n", total_turn_time/processes.size());
-    printf("Average waiting time: %f \n", total_wait_time/processes.size());
+    // total time / # processes
+    printf("\tOverall average: %f seconds\n", (total_time/1000.0)/processes.size());
+    printf("Average turnaround time: %f seconds\n", total_turn_time/processes.size());
+    printf("Average waiting time: %f seconds\n", total_wait_time/processes.size());
 }
 
 uint32_t currentTime()
